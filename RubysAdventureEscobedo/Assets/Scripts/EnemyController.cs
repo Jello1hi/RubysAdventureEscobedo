f560667d035d8181e29f9aa34c4f4b568d129dc3
Jello1hi <709961@student.dusd.net>
2022-12-01 11:44:32 -0800

    did animation started projectile
    animation is messed up and im on the third one in projectile.

--- RubysAdventureEscobedo/Assets/Scripts/EnemyController.cs
+++ RubysAdventureEscobedo/Assets/Scripts/EnemyController.cs
@@ -11,12 +11,14 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    Animator animator;
 
   
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -36,10 +38,14 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction; ;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction; ;
         }
 
