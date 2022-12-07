d7fb70d0b04256455b8ee0f8356a5a29e26cbb8c
Jello1hi <709961@student.dusd.net>
2022-12-07 11:41:56 -0800

    finished o paricle and camera
    started ui health could not get the blue dot to move kms

--- RubysAdventureEscobedo/Assets/Scripts/EnemyController.cs
+++ RubysAdventureEscobedo/Assets/Scripts/EnemyController.cs
@@ -8,13 +8,16 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
     bool broken = true;
+
     Animator animator;
 
-  
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -24,10 +27,12 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
-        if(!broken)
+        
+        if (!broken)
         {
             return;
         }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -39,23 +44,25 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(!broken)
+       
+        if (!broken)
         {
             return;
         }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
         {
+            position.y = position.y + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", 0);
             animator.SetFloat("Move Y", direction);
-            position.y = position.y + Time.deltaTime * speed * direction; ;
         }
         else
         {
+            position.x = position.x + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
-            position.x = position.x + Time.deltaTime * speed * direction; ;
         }
 
         rigidbody2D.MovePosition(position);
@@ -64,17 +71,23 @@ public class EnemyController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
+
         if (player != null)
         {
             player.ChangeHealth(-1);
         }
     }
+
+    
     public void Fix()
     {
         broken = false;
         rigidbody2D.simulated = false;
+       
         animator.SetTrigger("Fixed");
 
+        smokeEffect.Stop();
     }
 }
- 
+
+
